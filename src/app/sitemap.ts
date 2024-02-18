a362e0edb9be3475a0ff0394561c8c7907056fd4
kaushalluffa <kaushalluffa91@gmail.com>
2024-02-17 19:32:32 -0500

    fix for routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,7 +8,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogPosts = await client.getAllByType("blog_post");
   const projects = await client.getAllByType("project");
 
-  const siteRoot = "https://demo.com";
+  const siteRoot = "https://kaushal-luffa.netlify.app";
 
   const homepageRoute = {
     url: siteRoot,
--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -30,7 +30,7 @@ export default function ContentList({
   const [hovering, setHovering] = useState(false);
   const lastMousePos = useRef({ x: 0, y: 0 });
 
-  const urlPrefix = contentType === "Blogs" ? "/blog" : "/project";
+  const urlPrefix = contentType === "Blogs" ? "/blog" : "/projects";
 
   useEffect(() => {
     // Animate list-items in with a stagger
